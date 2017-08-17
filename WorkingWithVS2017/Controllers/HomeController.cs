4fe458b6f3f2313094064c127e35bada8998ab9e
Ryan Transfiguracion <ryan_transfiguracion@outlook.com>
2017-08-17 04:42:18 -0700

    Making simple changes for the "Start without debugging" feature.

--- WorkingWithVS2017/Controllers/HomeController.cs
+++ WorkingWithVS2017/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WorkingWithVS2017.Models;
+using System.Linq;
 
 namespace WorkingWithVS2017.Controllers
 {
@@ -8,6 +9,7 @@ namespace WorkingWithVS2017.Controllers
   public class HomeController : Controller
   {
     public IActionResult Index()
-      => View(SimpleRepository.SharedRepository.Products);
+      => View(SimpleRepository.SharedRepository.Products
+              .Where(p => p.Price < 50));
   }
 }
